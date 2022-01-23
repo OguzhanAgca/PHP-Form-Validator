61d6bd9bee63c38c4a6229959532cb437745c2fd
Oğuzhan Ağca <oguzhanagca.oe@gmail.com>
2022-01-23 20:16:32 +0300

    Form Validator v1.0.0

---
+++ FormValidation.php
@@ -0,0 +1,90 @@
+<?php
+
+class FormValidation
+{
+    private $name;
+    private $value;
+    private $errorMsg = [];
+
+    public function name($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function value($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    public function validation($field)
+    {
+        switch ($field) {
+            case "name":
+            case "lastname":
+                if (!preg_match('/^[A-ZİĞÜŞÇÖa-züiçşğı]{2,20}$/', $this->value)) {
+                    $this->errorMsg[$this->name] = "{$this->name} field cannot contain spaces, must be between 2 and 20 characters and contain only letters.";
+                }
+                return $this;
+            case "email":
+                if (!filter_var($this->value, FILTER_VALIDATE_EMAIL) ? true : false) {
+                    $this->errorMsg[$this->name] = "Please enter a valid {$this->name}.";
+                }
+                return $this;
+            case "username":
+                if (!preg_match('/^[a-zA-Z][0-9a-zA-Z_]{2,23}[0-9a-zA-Z_-]$/', $this->value)) {
+                    $this->errorMsg[$this->name] = "{$this->name} is not valid.";
+                }
+                return $this;
+            case "password":
+                if (!preg_match('/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$/', $this->value)) {
+                    $this->errorMsg[$this->name] = "{$this->name} is not strong enough.";
+                }
+                return $this;
+        }
+    }
+
+    public function fileValidation(string $fileName, int $fileSize, int $allowedSize, array $allowedExt)
+    {
+        $ext = pathinfo($fileName, PATHINFO_EXTENSION);
+        $allowedSize = number_format($allowedSize / 1048576, 2, ".", null); // Byte -> Mb
+        $fileSize = number_format($fileSize / 1048576, 2, ".", null); // Byte -> Mb
+
+        if (!in_array($ext, $allowedExt)) {
+            $this->errorMsg[$this->name] = "You can not upload $ext files.";
+        }
+
+        if ($allowedSize < $fileSize) {
+            $this->errorMsg[$this->name] = "The file size cannot be larger than $allowedSize MB.";
+        }
+
+        return $this;
+    }
+
+    public function customValidation($pattern, $msg)
+    {
+        if (!preg_match($pattern, $this->value)) {
+            $this->errorMsg[$this->name] = $msg;
+        }
+
+        return $this;
+    }
+
+    public function required()
+    {
+        if (mb_strlen($this->value) <= 0) {
+            array_push($this->errorMsg, "Please fill in all fields.");
+        }
+        return $this;
+    }
+
+    public function isSuccess()
+    {
+        if (count($this->errorMsg) > 0) {
+            return $this->errorMsg;
+        } else {
+            return true;
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,49 @@
+## PHP Simple Form Validation
+
+You can do a simple form control in PHP!
+
+### Usage
+
+- Create a new object from FormValidation class.
+- Check form using chaining methods.
+
+### Code Template
+
+```php
+$formVal = new FormValidation();
+$formVal->name("PlaceholderName")->value(FormValue)->validation("name || lastname || username || email || password")->required();
+$formVal->name("PlaceholderName")->value(FormValue)->customValidation("/pattern/","message")-required();
+$formVal->name("PlaceholderName")->fileValidation(FileName,FileSize,AllowedSize,AllowedExt)->required();
+```
+
+### Code Example
+
+For example, I have a form with username, name, last name, email, password, and image fields.
+And I pass the data from the user through the security functions and store it in variables like below.
+
+```php
+$userName = $_POST["username"];
+$name = $_POST["name"];
+$lastName = $_POST["lastname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$file = $_FILES["myFile"];
+```
+
+Now, we can create an object from the FormValidation class as below and check the incoming data and print information to the user.
+
+```php
+$formVal = new FormValidation();
+$formVal->name("Username")->value($userName)->validation("username")->required();
+$formVal->name("Name")->value($name)->validation("name")->required();
+$formVal->name("Lastname")->value($lastName)->validation("lastname")->required();
+$formVal->name("E-mail")->value($email)->validation("email")->required();
+$formVal->name("Password")->value($password)->validation("password")->required();
+$formVal->name("Image")->>fileValidation($file["name"],$file["size"],1048576,["png","jpg"]);
+```
+
+If we want to do different control, we can use the customValidation() method in the FormValidation class.
+
+```php
+$formVal->name("Address")->value($address)->customValidation("/pattern/", "Your address is wrong..")->required();
+```
